2b72a9526cc391cc692dafe33251d1420609206b
CodeLuminary <victorijoni@yahoo.com>
2024-03-02 13:57:06 +0100

    Project updated

--- doctor/prescriptionSerializer.py
+++ doctor/prescriptionSerializer.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
-from .models import Prescription, PrescriptionItem
+from .models import Prescription, PrescriptionItem, Patient
+from pharmacist.models import Drug
+
+class DrugSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drug
+        fields = (
+            "__all__"
+        )
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = (
+            "__all__"
+        )
 
 class PrescriptionSerializers(serializers.ModelSerializer):
+    #message = PatientSerializer
     class Meta:
         model = Prescription
         fields = (
             "__all__"
-        )
+        ) #["full_name","purpose","number","age"]
 
 class PrescriptionItemSerializers(serializers.ModelSerializer):
     class Meta:
@@ -14,3 +30,20 @@ class PrescriptionItemSerializers(serializers.ModelSerializer):
         fields = (
             "__all__"
         )
+
+class PrescriptionResultSerializer(serializers.ModelSerializer):
+    patient = PatientSerializer(many=False)
+    #prescription = PrescriptionSerializers(many=False)
+    class Meta:
+        model = Prescription
+        fields = (
+            "__all__"
+        ) 
+
+class PrescriptionItemResultSerializer(serializers.ModelSerializer):
+    drug_id = DrugSerializer(many=False)
+    class Meta:
+        model = PrescriptionItem
+        fields = (
+            "__all__"
+        ) 
--- pharmacist/views.py
+++ pharmacist/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .addDrugForm import AddDrugForm
 from .models import Drug
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from doctor.models import Prescription, PrescriptionItem
+from doctor.prescriptionSerializer import PrescriptionSerializers, PrescriptionItemSerializers, PrescriptionResultSerializer, PrescriptionItemResultSerializer
 
-
+def getDrugs():
+    return Drug.objects.all()
 
 # Create your views here.
 def index(request):
-    return render(request, 'pharmacist/index.html',{})
+    return render(request, 'pharmacist/index.html',{"data": getDrugs()})
 
 def addDrug(request):
     form = AddDrugForm(request.POST)
@@ -25,7 +30,7 @@ def addDrug(request):
             return render(request, "pharmacist/index.html", {
                 "status": True,
                 "message": "Drug added successfully",
-                "data": newDrug.id
+                "data": getDrugs()
             })
 
         except:
@@ -33,9 +38,21 @@ def addDrug(request):
             return render(request, "pharmacist/index.html", {
                "status": False,
                "message": "Action failed. Drug could not be added",
+               "data": getDrugs()
             })
     
     else: 
         #print('horible')
-        return render(request, "pharmacy/index.html",{})
-    
+        return render(request, "pharmacy/index.html",{"data": getDrugs()})
+
+@api_view(['POST'])
+def searchPrescription(request): 
+    prescript = Prescription.objects.select_related('patient').get(prescription_id=request.data['text']) #.seletect_related('Patient')
+    print(prescript)
+    presItems = PrescriptionItem.objects.filter(prescription=prescript.id)
+
+    presSer = PrescriptionResultSerializer(prescript) #PrescriptionSerializers(prescript)
+    press = PrescriptionItemResultSerializer(presItems, many=True)
+    print(presItems)
+    return Response({"prescription": presSer.data, "drugs": press.data})
+        
